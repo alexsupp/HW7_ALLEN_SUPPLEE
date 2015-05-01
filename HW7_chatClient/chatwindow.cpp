73f4e475ace6cb70de20e3934cfeca6563c3cdbd
Alex S <alexsupp@vt.edu>
2015-05-01 18:41:27 -0400

    Created the chatWindow which includes files chatwindow.cpp, chatwindow.h, and chatwindow.ui.

--- HW7_chatClient/chatwindow.cpp
+++ HW7_chatClient/chatwindow.cpp
@@ -1,5 +1,6 @@
 #include "chatwindow.h"
 #include "ui_chatwindow.h"
+#include <QString>
 
 chatWindow::chatWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -11,4 +12,20 @@ chatWindow::chatWindow(QWidget *parent) :
 chatWindow::~chatWindow()
 {
     delete ui;
+}
+
+void chatWindow::on_sendButton_clicked()
+{
+    QString msg = ui->lineEditMessage->text();
+
+    if(msg.isEmpty())
+        return;
+
+    ui->textEditMessages->append("you: " + msg);
+    ui->lineEditMessage->clear();
+}
+
+void chatWindow::on_lineEditMessage_textChanged(const QString &arg1)
+{
+    ui->msgSizeLabel->setText(QString::number(arg1.size())+"/255");
 }
